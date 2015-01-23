a70b89945b56ae2367e8ddc29709de352c954a6c
AutumnSpring <nazhang34@163.com>
2015-01-23 23:00:34 +0800

    PythonForFun

---
+++ demoRunPythonUnderWindows.py
@@ -0,0 +1,8 @@
+import platform;
+
+pythonVersion=platform.python_version();
+uname=platform.uname();
+
+print "Just for demo how to do python development under windows:";
+print "Current python version info is %s"%(pythonVersion);
+print "uname=",uname;
---
+++ list_and_for.py
@@ -0,0 +1,4 @@
+#4_1_list_and_for
+list=[1,'one',2,True]
+for item in list:
+  print(item)
